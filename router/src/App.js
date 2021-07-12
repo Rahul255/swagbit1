e8e917c208e883f9fc3fe6344ab2ca9cfa5b5209
Rahul255 <rahulmohan255@gmail.com>
2021-07-12 22:43:06 +0530

    file added

--- router/src/App.js
+++ router/src/App.js
@@ -1,9 +1,22 @@
 
 import './App.css';
+import About from './Container/About';
+import Profile from './Container/Profile';
+import {useState} from 'react';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 
 function App() {
+  const [state,setState] = useState('')
+
   return (
     <div className="App">
+      
+      <Router>
+        <Link to='/about'>About Page</Link>
+        <Link to='/profile'> profile Page</Link>
+        <Route component={About} path='/about' />
+        <Route component={Profile} path='/profile' />
+      </Router>
       
     </div>
   );
